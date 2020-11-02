bdda6e57a097ceb20656efa5331a27cdec217684
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-02 07:23:47 -0800

    Add interface.ts that demostrates the issue with object literal type annotations

---
+++ interfaces.ts
@@ -0,0 +1,18 @@
+const oldCivic = {
+  name: 'civic',
+  year: 2000,
+  broken: true
+};
+
+
+// Problem with vehicle with object literal type annotation
+// It's hard to read the type, if we have other functions that take vehicle 
+// then we'd have to duplicate this object literal type annotation.
+// Not very nice.
+const printVehicle = (vehicle: { name: string; year: number; broken: boolean }): void => {
+  console.log(`Name: ${vehicle.name}`);
+  console.log(`Year: ${vehicle.year}`);
+  console.log(`Broken?: ${vehicle.broken}`);
+};
+
+printVehicle(oldCivic);
