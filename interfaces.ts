f511e6e9c295e3d60a3024a811bbcf5a3462e6c2
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-02 07:28:15 -0800

    Create and use interface

--- interfaces.ts
+++ interfaces.ts
@@ -1,3 +1,10 @@
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
+
 const oldCivic = {
   name: 'civic',
   year: 2000,
@@ -9,7 +16,15 @@ const oldCivic = {
 // It's hard to read the type, if we have other functions that take vehicle 
 // then we'd have to duplicate this object literal type annotation.
 // Not very nice.
-const printVehicle = (vehicle: { name: string; year: number; broken: boolean }): void => {
+// const printVehicle = (vehicle: { name: string; year: number; broken: boolean }): void => {
+//   console.log(`Name: ${vehicle.name}`);
+//   console.log(`Year: ${vehicle.year}`);
+//   console.log(`Broken?: ${vehicle.broken}`);
+// };
+
+
+// Using Interface
+const printVehicle = (vehicle: Vehicle): void => {
   console.log(`Name: ${vehicle.name}`);
   console.log(`Year: ${vehicle.year}`);
   console.log(`Broken?: ${vehicle.broken}`);
