fb63b1726b0d6f17eaf53747468fceb19072b5a1
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-21 08:28:12 -0800

    Add generics with functions

--- generics.ts
+++ generics.ts
@@ -31,3 +31,30 @@ const arr2 = new ArrayOfAnything<number>([1, 2, 3]);
 // Using type inference with generics
 const arr11 = new ArrayOfAnything(['abc', 'def', 'ghi']);
 const arr22 = new ArrayOfAnything([1, 2, 3]);
+
+
+// Example of generics with functions
+
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+// Using generics for function's param
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<string>(['abc', 'def', 'ghi']);
+
+// Using generics with type inference
+printAnything(['abc', 'def', 'ghi']);
