6020013a687f8bd21afaab010566886db3aa6e66
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-11 17:40:44 -0800

    Add metadata to plane's color property

--- metadata.ts
+++ metadata.ts
@@ -4,10 +4,16 @@ const plane = {
   color: 'red'
 };
 
-Reflect.defineMetadata('note', 'hi there', plane);
-Reflect.defineMetadata('height', 10, plane);
+// Reflect.defineMetadata('note', 'hi there', plane);
+// Reflect.defineMetadata('height', 10, plane);
+// 
+// const note = Reflect.getMetadata('note', plane);
+// const height = Reflect.getMetadata('height', plane);
+// console.log(note);
+// console.log(height);
 
-const note = Reflect.getMetadata('note', plane);
-const height = Reflect.getMetadata('height', plane);
+Reflect.defineMetadata('note', 'hi there', plane, 'color'); // associate metat data to plane's color property
+
+const note = Reflect.getMetadata('note', plane, 'color');
+
 console.log(note);
-console.log(height);
