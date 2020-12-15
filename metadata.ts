82bca97271aa695fe8ce4e97d83c5f078c4548ed
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-15 07:12:27 -0800

    Rename decorators

--- metadata.ts
+++ metadata.ts
@@ -18,26 +18,28 @@ import 'reflect-metadata'
 // 
 // console.log(note);
 
-@printMetadata
+@controller
 class Plane {
   color: string = 'red';
 
-  @markFunction('Hi There')
+  @get('/login')
   fly(): void {
     console.log('vrrrrrr');
   }
 }
 
-function markFunction(secretInfo: string) {
+function get(path: string) {
   return function (target: Plane, key: string) {
-    Reflect.defineMetadata('secret', secretInfo, target, key);
+    Reflect.defineMetadata('path', path, target, key);
   }
 }
 
-function printMetadata(target: typeof Plane) {
+function controller(target: typeof Plane) {
   for (let key in target.prototype) {
-    const secret = Reflect.getMetadata('secret', target.prototype, key);
-    console.log(secret);
+    const path = Reflect.getMetadata('path', target.prototype, key);
+    const middleware = Reflect.getMetadata('middleware', target.prototype, key);
+
+    router.get(path, target.prototype[key]);
   }
 }
 
